a4faa938bf7c6efedf573aab9e087527cf418c53
mrnakumar <nknkchoudhary@gmail.com>
2021-05-15 01:40:39 +0200

    Filter out polls that are still open
    Only polls that are closed for voting should be kept.
    Any other poll will be ignored.

--- poller/poller.go
+++ poller/poller.go
@@ -8,7 +8,23 @@ func Fetch(client TwitterClient, bearer string, tweetId string) {
 	url := fmt.Sprintf(pollFetchUrl, tweetId)
 	resp, err := client.GetPoll(url, bearer)
 	if err == nil {
+		fmt.Printf("%+v\n", resp)
+		fmt.Printf("After.....\n")
+		keepOnlyClosed(&resp)
 		fmt.Printf("%+v", resp)
+	} else {
+		fmt.Printf("%v", err)
 	}
-	fmt.Printf("%v", err)
+}
+
+func keepOnlyClosed(resp *PollFetchResponse) {
+	if resp.Includes.Polls != nil {
+		copy := resp.Includes.Polls[:0]
+		for _, poll := range resp.Includes.Polls {
+			if poll.VotingStatus == "closed" {
+				copy = append(copy, poll)
+			}
+		}
+		resp.Includes.Polls = copy
+	}
 }
